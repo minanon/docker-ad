9c00e7acd5ca112b20b2761325b72280bffdc7ae
Sumi Minano <sumi.minano@gmail.com>
2016-01-06 05:36:59 +0000

    useradd

--- add_files/scripts/passwd-expire.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# @(#) edit password expire. It is as same as "samba-tool domain passwordsettings set --max-pwd-age=${1}".
-
-/scripts/samba-tool.sh domain passwordsettings set --max-pwd-age="${1}"
--- add_files/scripts/useradd.sh
+++ add_files/scripts/useradd.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # @(#) add samba user. It is as same as "samba-tool user create".
 
+set -eu
+
 /scripts/samba-tool.sh user create "${@}"
+
+samba-tool user setexpiry "${1}" --noexpiry
